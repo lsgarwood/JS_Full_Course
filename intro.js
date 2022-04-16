68c0f52fc020cf84b91e4ae06db95fe9aa0e893b
Lauren <ls_garwood@yahoo.co.uk>
2022-04-16 14:34:44 +0100

    LG: intro

---
+++ intro.js
@@ -0,0 +1,23 @@
+console.log("Hello freecodecamp");
+
+// inline comments
+
+/* multiline
+comment
+*/
+
+/* Data types
+data is anything that is useful to the computer
+undefined, null, boolean, string, symbol, number, and object
+*/
+
+// variable is a pointer that points to data
+
+var myName = "Beau"; // used through whole programme
+myName = 8;
+console.log(myName);
+ 
+let ourName = "freecodecamp"; //only where it has been declared
+
+const pi = 3.14; // cannot reassign
+
