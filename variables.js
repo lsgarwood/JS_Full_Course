55242fd3bb5f7a97ccd3446f92be43727cc4ecbf
Lauren <ls_garwood@yahoo.co.uk>
2022-04-16 14:47:45 +0100

    LG: Variables intro

---
+++ variables.js
@@ -0,0 +1,19 @@
+//differene between declaring and assigning variables
+
+var a; // declared
+console.log(a);
+
+var b = 2;  // assigned
+
+a = 7;
+b = a;
+
+console.log(a); // now chsanged to 7
+console.log(b); // now the same
+
+//initialising variable with assignment operators
+
+var c = 10; // equals is initialising
+
+//case sensitive
+
